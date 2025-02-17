29becb75101cab15fea4073d3c36b25de7d9f44a
atharva-2408 <atharva@tranquilsys.com>
2025-02-17 11:46:47 +0530

    created updateTask Api

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTask, getTaskById, getTasks } from '../controllers/taskController.js';
+import { createTask, getTaskById, getTasks, updateTask } from '../controllers/taskController.js';
 
 // router object
 const router = express.Router();
@@ -14,5 +14,8 @@ router.get('/get/:taskId', getTaskById);
 
 // CREATE TASK || POST 
 router.post('/create', createTask);
+
+//  UPDATE TASK || POST
+router.put('/update/:taskId', updateTask);
 
 export default router;
