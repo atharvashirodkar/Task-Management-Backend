82fdea23d100e2713692d3885f87f88ba6a5f9ff
atharva-2408 <atharva@tranquilsys.com>
2025-02-17 10:56:06 +0530

    created getById controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,21 +1,22 @@
-import db from "../config/db";
+import db from "../config/db.js";
 
-// GET ALL TASK LIST
-const getTasks = async (req, res) =>{
-    try{
+// // GET ALL TASK LIST
+const getTasks = async (req, res) => {
+    try {
         const data = await db.query("SELECT * FROM tasks ");
         if (!data) {
             return res.status(404).send({
                 success: false,
                 message: "Task not found"
             })
-        } 
+        }
         res.status(200).send({
             success: true,
             message: "Task List",
-            data
+            totalTasks: data[0].length,
+            data: data[0]
         });
-    } catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send({
             success: false,
@@ -25,4 +26,35 @@ const getTasks = async (req, res) =>{
     }
 };
 
-export {getTasks};
+const getTaskById = async (req, res) => {
+    try {
+        const taskId = req.params.taskId;
+        if (!taskId) {
+            return res.status(400).send({
+                success: false,
+                message: "Invalid or Provide Task Id",
+            });
+        }
+        const data = await db.query("SELECT * FROM tasks WHERE id =?", [taskId]);
+        if (!data[0][0]) {
+            return res.status(404).send({
+                success: false,
+                message: "Task not found"
+            });
+        }
+        res.status(200).send({
+            success: true,
+            message: "Task Details",
+            data: data[0][0]
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Get Task By Id API",
+            error
+        });
+    }
+}
+
+export { getTasks, getTaskById };
